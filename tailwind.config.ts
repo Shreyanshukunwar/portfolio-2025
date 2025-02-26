9955677abb15a25336f10a007eb49725e173cd49
Shreyanshu Kunwar <shreyanshukunwar@gmail.com>
2025-02-26 17:01:19 +0545

    initial setup

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        default: '#000',
+        100: '#000319'
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
