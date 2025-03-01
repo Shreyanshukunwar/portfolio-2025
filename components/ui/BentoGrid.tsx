eb8efce5c01e3ba17baa2c5c73c1ef989a2f1eec
Shreyanshu Kunwar <shreyanshukunwar@gmail.com>
2025-03-01 09:40:16 +0545

    added tech stack and email copy components in bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,13 @@
+'use client'
+
+import { useState } from "react";
+import { IoCopyOutline } from "react-icons/io5";
+import Lottie from "react-lottie";
+
 import { cn } from "../../lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBG";
+import animationData from '../../data/confetti.json'
+import MagicButton from "./MagicButton";
 
 export const BentoGrid = ({
   className,
@@ -41,6 +49,18 @@ export const BentoGridItem = ({
   titleClassName? :string,
   spareImg? :string
 }) => {
+
+  const leftLists = ["ReactJS", "Express", "Typescript"];
+  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    const text = "shreyanshukunwar@gmail.com";
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
+
   return (
     <div
       className={cn(
@@ -52,7 +72,7 @@ export const BentoGridItem = ({
         backgroundColor: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)"
       }}
     >
-        <div className={`${id == 6} && 'flex justify-center h-full`}>
+        <div className={`${id === 6 && 'flex justify-center'} h-full`}>
             <div className="w-full h-full absolute">
             {img && (
                 <img
@@ -93,9 +113,63 @@ export const BentoGridItem = ({
                 <div className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}>
                     {title}
                 </div>
+            
 
+            {/* Tech stack list div */}
+            {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              {/* tech stack lists */}
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {leftLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                {rightLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
             </div>
+          )}
+
+          { id == 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie options={{
+                  loop: copied,
+                  autoplay: copied,
+                  animationData: animationData,
+                  rendererSettings : {
+                    preserveAspectRatio: 'xMidYMid slice'
+                  }
+                }}/>
+              </div>
+
+              <MagicButton
+                title={copied ? "Email is Copied!" : "Copy my email address"}
+                icon={<IoCopyOutline />}
+                position="left"
+                handleClick={handleCopy}
+                otherClasses="!bg-[#161A31]"
+              />
+            </div>
+          )}
         </div>
+    </div>
     </div>
   );
 };
