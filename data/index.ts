89d144924302ae6115d3fe0af902125f65882590
Shreyanshu Kunwar <shreyanshukunwar@gmail.com>
2025-03-03 09:39:57 +0545

    fixed typo

--- data/index.ts
+++ data/index.ts
@@ -38,7 +38,7 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Tech enthusiast with a passion for reserach and development.",
+      title: "Tech enthusiast with a passion for research and development.",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
